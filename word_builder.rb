a5ff4d8dfeeb2ecd9df74b7c71ab877bad24957a
Srijayanth Sridhar <srijayanth@gmail.com>
2016-06-20 16:16:56 +0530

    Added a line number

--- word_builder.rb
+++ word_builder.rb
@@ -1,5 +1,7 @@
 words=[]
+word_count=1
 while true
+  print "#{word_count} "
   word=STDIN.readline.strip
   if (words.include? word)
     puts " -> #{word} already used"
@@ -7,6 +9,7 @@ while true
     puts " -> #{word} doesn't chain with #{words[-1]}"
   else
     words << word
+    word_count+=1
   end
 end
 
